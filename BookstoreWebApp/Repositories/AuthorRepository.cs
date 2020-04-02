d2764df540c840be775cf1da9c4819ab583be78b
fazerito <gervvaz@gmail.com>
2020-04-02 20:12:48 +0200

    add book, author and publisher repositories

---
+++ BookstoreWebApp/Repositories/AuthorRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookstoreWebApp.Data;
+using BookstoreWebApp.Models;
+
+namespace BookstoreWebApp.Repositories
+{
+    public class AuthorRepository : IRepository<Author>
+    {
+        private readonly BookstoreDBContext _context;
+
+        public AuthorRepository(BookstoreDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Author>> GetAll()
+        {
+            return await _context.Authors
+                .Include(a => a.Book)
+                .ToListAsync();
+        }
+
+        public async Task<Author> Get(int id)
+        {
+            return await _context.Authors
+                .Include(a => a.Book)
+                .FirstOrDefault(a => a.Id == id);
+        }
+
+        public async Task<Author> Add(Author entity)
+        {
+            _context.BookAuthors.Attach(entity.Book);
+            _context.Authors.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<Author> Update(Author entity)
+        {
+            if (!(await _context.Authors.AnyAsync(a => a.Id == entity.Id)))
+                throw new InvalidOperationException("Book does not exist");
+            
+            _context.Authors.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<Author> Delete(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+            if (author == null)
+                throw new InvalidOperationException("Author does not exist");
+            _context.Authors.Remove(author);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BookstoreWebApp/Repositories/BookRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookstoreWebApp.Data;
+using BookstoreWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookstoreWebApp.Repositories
+{
+    public class BookRepository : IRepository<Book>
+    {
+        private readonly BookstoreDBContext _context;
+
+        public BookRepository(BookstoreDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Book>> GetAll()
+        {
+            return await _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.Genre)
+                .Include(b => b.Publisher)
+                .Include(b => b.Storage)
+                .ToListAsync();
+        }
+
+        public async Task<Book> Get(int id)
+        {
+            return await _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.Genre)
+                .Include(b => b.Publisher)
+                .Include(b => b.Storage)
+                .FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task<Book> Add(Book entity)
+        {
+            if (!(await _context.Publishers.AnyAsync(p => p == entity.Publisher)))
+                _context.Publishers.Add(entity.Publisher);
+            else
+                _context.Publishers.Attach(entity.Publisher);
+            if (!(await _context.Authors.AnyAsync(a => a == entity.Author)))
+                _context.Authors.Add(entity.Author);
+            else
+                _context.BookAuthors.Attach(entity.Author);
+
+            _context.Books.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<Book> Update(Book entity)
+        {
+            if (!(await _context.Books.AnyAsync(b => b.Id == entity.Id)))
+                throw new InvalidOperationException("Book does not exist");
+            _context.Books.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<Book> Delete(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+                throw new InvalidOperationException("Book does not exist");
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
