bbc97c30ce84d2d658142778daa36db7209ccbb3
Dmitry Korsak <40791556+Lebrenzi@users.noreply.github.com>
2020-04-07 22:00:18 +0200

    Book upload with photo
    Proste dodawanie książek wraz z uploadem zdjęcia(jednego) na serwer aplikacji. Widok + dependencies dla ksiązki to be done

---
+++ BookstoreWebApp/Controllers/BookController.cs
@@ -0,0 +1,70 @@
+﻿using BookstoreWebApp.Data;
+using BookstoreWebApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BookstoreWebApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookstoreDbContext _context;
+        IWebHostEnvironment _appEnvironment;
+        public BookController(BookstoreDbContext context, IWebHostEnvironment appEnvironment)
+        {
+            _context = context;
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Standardowy widok aplikacji
+        /// </summary>
+        /// <returns>Widok dodawania produktów</returns>
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Stworzenie ksiazki w bazie danych na podstawie podstawowych parametrów z modelu
+        /// </summary>
+        /// <returns>Widok dodawania produktów</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Price,Description,ISBN,Dating,Photo,AuthorId,GenreId,PublisherId")] Book newBook, IFormFile bookPhoto)
+        {
+            if (ModelState.IsValid)
+            {
+                if (bookPhoto == null)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    string path = "/Files/" + bookPhoto.FileName;
+                    /*var path = Path.GetFileNameWithoutExtension(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", PhotoName.FileName));
+                    var stream = new FileStream(path, FileMode.Create);
+                    await PhotoName.CopyToAsync(stream);*/
+                    newBook.Photo = path;
+                    using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                    {
+                        await bookPhoto.CopyToAsync(fileStream);
+                    }
+                }
+                newBook.Genre = _context.Genres.Find(newBook.GenreId);
+                newBook.Author = _context.Authors.Find(newBook.AuthorId);
+                newBook.Publisher = _context.Publishers.Find(newBook.PublisherId);
+                _context.Add(newBook);
+                await _context.SaveChangesAsync();
+            }
+            return View();
+        }
+    }
+}
