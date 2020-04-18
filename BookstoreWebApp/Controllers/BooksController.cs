e0a35ed8aed8a0265c475952994c2e627ea8bb51
fazerito <gervvaz@gmail.com>
2020-04-18 17:01:02 +0200

    add views for book, update homepage and navbar

--- BookstoreWebApp/Controllers/BooksController.cs
+++ BookstoreWebApp/Controllers/BooksController.cs
@@ -1,13 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using BookstoreWebApp.Helpers;
+using BookstoreWebApp.Models;
+using BookstoreWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookstoreWebApp.Controllers
 {
     public class BooksController : Controller
     {
-        // GET
-        public IActionResult Index()
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
         {
-            return View();
+            _bookService = bookService;
+        }
+        
+        public async Task<IActionResult> Index(string currentFilter, string searchString, int? pageNumber)
+        {
+            if (searchString != null)
+            {
+                pageNumber = 1;
+                return View(await PaginatedList<Book>.CreateAsync(_bookService.GetFiltered(searchString),
+                    pageNumber ?? 1, 20));
+            }
+            searchString = currentFilter;
+            
+            ViewData["CurrentFilter"] = searchString;
+            int pageSize = 20;
+            return View(await PaginatedList<Book>.CreateAsync(_bookService.GetAll(), pageNumber ?? 1, pageSize));
         }
     }
 }
