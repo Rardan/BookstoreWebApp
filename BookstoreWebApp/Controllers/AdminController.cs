08b603a0f236b1112146ebe6c901042232c7512e
Rardan <przemolex01@gmail.com>
2020-06-02 11:21:11 +0200

    Admin & Employee Controllers and Views + bug fix

---
+++ BookstoreWebApp/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookstoreWebApp.Models;
+using BookstoreWebApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreWebApp.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<StoreUser> _userManager;
+
+        public AdminController(UserManager<StoreUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RegisterEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterEmployee(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new StoreUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if(result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, "Employee");
+                    ViewBag.CreateUserSucceeded = "Created Employee: " + user.Email;
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ BookstoreWebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookstoreWebApp.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreWebApp.Controllers
+{
+    [Authorize(Roles = "Employee,Admin")]
+    public class EmployeeController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public EmployeeController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ManageOrderStatus()
+        {
+            var orders = _orderRepository.Orders;
+            return View(orders);
+        }
+
+        public async Task<RedirectToActionResult> ChangeOrderStatus(string orderNumber)
+        {
+            var order = _orderRepository.GetOrderByNumber(orderNumber);
+
+            if (order != null)
+            {
+                if(order.Condidtion == "Created")
+                {
+                    _orderRepository.ChangeOrderStatus(order, "Packed");
+                }
+                else if (order.Condidtion == "Packed")
+                {
+                    _orderRepository.ChangeOrderStatus(order, "Shipped");
+                }
+            }
+            return RedirectToAction("ManageOrderStatus");
+        }
+    }
+}
